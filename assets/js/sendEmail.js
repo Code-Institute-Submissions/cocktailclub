d09f3468b66bb065175534189d0ad0312277c817
fayskerritt <fayskerritt1@gmail.com>
2020-07-29 12:35:13 +0000

    Added in emailJS and tested to ensure emails are sent to proper email address in correct format

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "cocktailContact", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "request": contactForm.request.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
